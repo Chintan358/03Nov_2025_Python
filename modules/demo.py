c2805bc2faeb80319b044bc626a54c66323bda88
chintan <chintan@gmail.com>
2025-12-13 08:50:56 +0530

    module2

--- modules/demo.py
+++ modules/demo.py
@@ -1,15 +1,18 @@
-import calc as c
-import test as t
-# from test import *
-# from calc import *
+# import calc as c
+# import test as t
+# # from test import *
+# # from calc import *
 
 
-# calc.add(10,20)
-# calc.square(10)
+# # calc.add(10,20)
+# # calc.square(10)
+
+# # c.add(10,20)
+# # c.square(10)
 
 # c.add(10,20)
 # c.square(10)
+# t.add(10,20,30)
 
-c.add(10,20)
-c.square(10)
-t.add(10,20,30)
+# from arithmatic import calc
+# calc.add(10,20)

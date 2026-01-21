f3f8e27e99fc2aff34404d15d1428b3c170506c0
chintan <chintan@gmail.com>
2026-01-21 13:30:22 +0530

    all

--- 013_Django/myapp/admin.py
+++ 013_Django/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from myapp.models import *
 # Register your models here.
+
+class StudentData(admin.ModelAdmin):
+    list_display=["id","name","email","phone","age","fees","dob","info","gender"]

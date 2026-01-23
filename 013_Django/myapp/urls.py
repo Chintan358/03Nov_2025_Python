fad8a6403ae67231b9a18a9f6e153b2e76dc2b22
chintan <chintan@gmail.com>
2026-01-23 09:13:32 +0530

    django

--- 013_Django/myapp/urls.py
+++ 013_Django/myapp/urls.py
@@ -3,5 +3,8 @@ from myapp.views import *
 
 urlpatterns = [
     path("",index,name="index"),
-    path("reg",reg,name="reg")
+    path("reg",reg,name="reg"),
+    path("display",display,name="display"),
+    path("delete",pro_delete,name="delete"),
+    path("byid",by_id,name="byid")
 ]
--- 013_Django/myapp/views.py
+++ 013_Django/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from myapp.models import *
 # Create your views here.
 def index(request):
@@ -7,9 +7,38 @@ def index(request):
 def reg(request):
     if request.method=='POST':
         data = request.POST
+        id = data['id']
         name = data['name']
         price = data['price']
-        qty = data['qty']
+        qty = data['qty'] 
 
-            
-        return render(request,"index.html",{"msg":"Product added successfully !!!"})
+        if id :
+            pro = Product.objects.get(pk=id)
+            pro.name = name
+            pro.price = price
+            pro.qty=qty
+            pro.save()
+            return render(request,"index.html",{"msg":"Product updated successfully !!!"})
+        else:
+            Product.objects.create(name=name,price=price,qty=qty)
+            return render(request,"index.html",{"msg":"Product added successfully !!!"})
+    
+
+def display(request):
+    products = Product.objects.all()
+    return render(request,"display.html",{"products":products})
+
+
+def pro_delete(request):
+    did = request.GET['did']
+    pro = Product.objects.get(pk=did)
+    pro.delete()
+    return redirect("display")
+
+
+def by_id(request):
+    pid = request.GET['pid']
+    pro = Product.objects.get(pk=pid)
+   
+    return render(request,"index.html",{"pro":pro})
+

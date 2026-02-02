a6670a2ef2bb27a58eb8a32d9f39683250f19f85
chintan <chintan@gmail.com>
2026-02-02 09:03:03 +0530

    all

--- 014_REaltionShip/myapp/urls.py
+++ 014_REaltionShip/myapp/urls.py
@@ -4,5 +4,7 @@ from myapp.views import *
 
 urlpatterns = [
     path("",index,name="index"),
-    path("view",view_product,name="view")
+    path("view",view_product,name="view"),
+    path('delete',delete_product,name="delete"),
+    path('edit',edit_product,name="edit")
 ]

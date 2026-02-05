f336169d53b88ac64a9ecc190c1d2d00f150e0f4
chintan <chintan@gmail.com>
2026-02-05 08:59:00 +0530

    ajax

--- 015_Ajax/ajax/crudapp/urls.py
+++ 015_Ajax/ajax/crudapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from crudapp.views import *
 
 urlpatterns = [
-    path("",index,name="index1")
+    path("",index,name="index1"),
+    path("reg",reg,name="reg"),
+    path("display",display,name="display"),
+    path("delete",delete_student,name="delete"),
+    path("stbyid",stbyid,name="stbyid")
 ]
--- 015_Ajax/ajax/crudapp/views.py
+++ 015_Ajax/ajax/crudapp/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-
+from crudapp.models import *
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
 def index(request):
     return render(request,"home.html")
+
+def reg(request):
+    if request.method=="POST":
+        data = request.POST
+        name = data.get("name")
+        email = data.get("email")
+        phone = data.get("phone")
+
+        Student.objects.create(name=name,email=email,phone= phone)
+
+        return HttpResponse("Registration successfully !!!")
+    
+def display(request):
+    students = Student.objects.all()
+    return JsonResponse({"students":list(students.values())})
+
+def delete_student(request):
+    sid = request.GET['sid']
+    student = Student.objects.get(pk=sid)
+    student.delete()
+    return HttpResponse("Student deleted !!!")
+
+def stbyid(request):
+    sid = request.GET['sid']
+    student = Student.objects.filter(id=sid)
+    return JsonResponse({"student":list(student.values())})
